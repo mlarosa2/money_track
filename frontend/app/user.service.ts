b509feeea5ac756983b12d1f7f2eb88dfb693cc7
Mark LaRosa <mark.larosa2@gmail.com>
2016-09-16 17:20:40 -0400

    create user service

---
+++ frontend/app/user.service.ts
@@ -0,0 +1,25 @@
+import { Injectable }    from '@angular/core';
+import { Headers, Http } from '@angular/http';
+
+import 'rxjs/add/operator/toPromise';
+
+import { User } from './user';
+
+@Injectable()
+export class UserService {
+  private userUrl = 'http://localhost:3000/api/users';
+  private headers = new Headers({'Content-Type': 'application/json'});
+  constructor(private http: Http) {}
+
+  signUp(email: string, password: string): Promise<User> {
+    return this.http
+      .post(this.userUrl, JSON.stringify({"email": email, "password": password}), {headers: this.headers})
+      .toPromise()
+      .then(res => res.json().dat)
+      .catch(this.handleError);
+  }
+  private handleError(error: any): Promise<any> {
+    console.erorr('Error!', error);
+    return Promise.reject(error.message || error);
+  }
+}
