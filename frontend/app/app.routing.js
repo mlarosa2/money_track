1619be2e551e82d7767c3442b630ab9d0137095c
Mark LaRosa <mark.larosa2@gmail.com>
2016-09-21 12:36:18 -0400

    update routes to include login and default route

--- frontend/app/app.routing.js
+++ frontend/app/app.routing.js
@@ -1,10 +1,20 @@
 "use strict";
 var router_1 = require('@angular/router');
+var app_component_1 = require('./app.component');
 var signup_component_1 = require('./signup.component');
+var login_component_1 = require('./login.component');
 var appRoutes = [
+    {
+        path: '',
+        component: app_component_1.AppComponent
+    },
     {
         path: 'signup',
         component: signup_component_1.SignupComponent
+    },
+    {
+        path: 'login',
+        component: login_component_1.LoginComponent
     }
 ];
 exports.routing = router_1.RouterModule.forRoot(appRoutes);
--- frontend/app/app.routing.ts
+++ frontend/app/app.routing.ts
@@ -1,11 +1,22 @@
 import { ModuleWithProviders } from '@angular/core';
-import { Routes, RouterModule }      from '@angular/router';
-import { SignupComponent } from './signup.component'
+import { Routes, RouterModule } from '@angular/router';
+import { AppComponent } from './app.component';
+import { SignupComponent } from './signup.component';
+import { LoginComponent } from './login.component';
+
 
 const appRoutes: Routes = [
   {
+    path: '',
+    component: AppComponent
+  },
+  {
     path: 'signup',
     component: SignupComponent
+  },
+  {
+    path: 'login',
+    component: LoginComponent
   }
 ];
 
