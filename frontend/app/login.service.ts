f041cdee9324264143834bf3319ac524eb56f57d
Mark LaRosa <mark.larosa2@gmail.com>
2016-09-21 12:33:41 -0400

    create and compile login service

---
+++ frontend/app/login.service.ts
@@ -0,0 +1,18 @@
+import { Injectable } from '@angular/core';
+import { Http, Headers, Response } from '@angular/http';
+import 'rxjs/add/operator/toPromise';
+
+@Injectable()
+export class LoginService {
+  public token: string;
+  private loginUrl: string = 'http://localhost:3000/api/session';
+  private headers = new Headers({'Content-Type': 'application/json'});
+  constructor(private http: Http) {}
+
+  login(email: string, password: string): Promise<any> {
+    return this.http
+      .post(this.loginUrl, JSON.stringify({"user": {"email": email, "password": password}}), {headers: this.headers})
+      .toPromise()
+      .then(res => console.log(res));
+  }
+}
