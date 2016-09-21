fdd9306d92fbbabead16df83aeadd74930ef674d
Mark LaRosa <mark.larosa2@gmail.com>
2016-09-21 12:32:59 -0400

    creat and compile login component

---
+++ frontend/app/login.component.ts
@@ -0,0 +1,19 @@
+import { Component } from '@angular/core';
+import { Router } from '@angular/router';
+import { LoginService } from './login.service';
+
+@Component({
+  selector: 'log-in',
+  templateUrl: 'app/login.component.html',
+  providers: [LoginService]
+})
+
+export class LoginComponent {
+  constructor(private loginService: LoginService,
+              private router: Router) {}
+  login(email: string, password: string): void {
+    email = email.trim();
+    password = password.trim();
+    this.loginService.login(email, password);
+  }
+}
