28ab3d63ee4b202779058d37f2ff4220725dd670
Mark LaRosa <mark.larosa2@gmail.com>
2016-09-21 15:49:13 -0400

    add index and create functionality

--- api_money_track/app/controllers/api/transactions_controller.rb
+++ api_money_track/app/controllers/api/transactions_controller.rb
@@ -1,2 +1,23 @@
 class TransactionsController < ApplicationController
+  def index
+    user_id = User.find_by(session_token: params[:transaction][:session_token]).id
+    @transactions = Transaction.where(user_id: user_id)
+    render @transactions
+  end
+
+  def create
+    user_id = User.find_by(session_token: params[:transaction][:session_token]).id
+    @transaction = Transaction.new(transaction_params, user_id)
+    if @transaction.save
+      render @transaction
+    else
+      render json: @transaction.errors.full_messages, status: 412
+    end
+  end
+
+
+  private
+  def transaction_params
+    params.require(:transaction).permit(:name, :type, :committed, :amount, :session_token, :month)
+  end
 end
