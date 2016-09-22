b7da1793812ca5e5122496c9ece76115d0f8dab1
Mark LaRosa <mark.larosa2@gmail.com>
2016-09-21 21:24:44 -0400

    create transaction service

---
+++ frontend/app/transaction.service.js
@@ -0,0 +1,40 @@
+"use strict";
+var __decorate = (this && this.__decorate) || function (decorators, target, key, desc) {
+    var c = arguments.length, r = c < 3 ? target : desc === null ? desc = Object.getOwnPropertyDescriptor(target, key) : desc, d;
+    if (typeof Reflect === "object" && typeof Reflect.decorate === "function") r = Reflect.decorate(decorators, target, key, desc);
+    else for (var i = decorators.length - 1; i >= 0; i--) if (d = decorators[i]) r = (c < 3 ? d(r) : c > 3 ? d(target, key, r) : d(target, key)) || r;
+    return c > 3 && r && Object.defineProperty(target, key, r), r;
+};
+var __metadata = (this && this.__metadata) || function (k, v) {
+    if (typeof Reflect === "object" && typeof Reflect.metadata === "function") return Reflect.metadata(k, v);
+};
+var core_1 = require('@angular/core');
+var http_1 = require('@angular/http');
+require('rxjs/add/operator/toPromise');
+var TransactionService = (function () {
+    function TransactionService(http) {
+        this.http = http;
+        this.transactionUrl = 'http://localhost:3000/api/transactions';
+        this.headers = new http_1.Headers({ 'Content-Type': 'application/json' });
+        this.sessionToken = JSON.parse(localStorage.getItem('currentUser')).session_token;
+        this.allTransactions = [];
+    }
+    TransactionService.prototype.create = function (type, committed, amount, name) {
+        return this.http
+            .post(this.transactionUrl, JSON.stringify({ "transaction": { "type": type, "committed": committed, "amount": amount, "name": name, "session_token": this.sessionToken } }), { headers: this.headers })
+            .toPromise()
+            .then(function (res) { return res.json().dat; })
+            .catch(this.handleError);
+    };
+    TransactionService.prototype.handleError = function (error) {
+        console.error('Error!', error);
+        return Promise.reject(error.message || error);
+    };
+    TransactionService = __decorate([
+        core_1.Injectable(), 
+        __metadata('design:paramtypes', [http_1.Http])
+    ], TransactionService);
+    return TransactionService;
+}());
+exports.TransactionService = TransactionService;
+//# sourceMappingURL=transaction.service.js.map
---
+++ frontend/app/transaction.service.ts
@@ -0,0 +1,27 @@
+import { Injectable } from '@angular/core';
+import { Headers, Http } from '@angular/http';
+
+import 'rxjs/add/operator/toPromise'
+
+import { Transaction } from './transaction';
+
+@Injectable()
+export class TransactionService {
+  private transactionUrl: string = 'http://localhost:3000/api/transactions';
+  private headers = new Headers({'Content-Type': 'application/json'});
+  private sessionToken: string = JSON.parse(localStorage.getItem('currentUser')).session_token;
+  private allTransactions = [];
+  constructor(private http: Http) {}
+
+  create(type: string, committed: string, amount: number, name: string): Promise<Transaction> {
+    return this.http
+      .post(this.transactionUrl, JSON.stringify({"transaction": {"type": type, "committed": committed, "amount": amount, "name": name, "session_token": this.sessionToken}}), {headers: this.headers})
+      .toPromise()
+      .then( res => res.json().dat)
+      .catch(this.handleError);
+  }
+  private handleError(error: any): Promise<any> {
+    console.error('Error!', error);
+    return Promise.reject(error.message || error);
+  }
+}
