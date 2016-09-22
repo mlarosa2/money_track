5f1e7c649de2ed1f262009b0d8525c90299e4583
Mark LaRosa <mark.larosa2@gmail.com>
2016-09-22 15:15:27 -0400

    add index function to transaction controller

--- api_money_track/app/controllers/api/transactions_controller.rb
+++ api_money_track/app/controllers/api/transactions_controller.rb
@@ -1,7 +1,8 @@
 class TransactionsController < ApplicationController
   def index
-    user_id = User.find_by(session_token: params[:transaction][:session_token]).id
-    @transactions = Transaction.get_by_month(user_id, params[:transaction][:month])
+    params.permit(:session_token, :month)
+    user_id = User.find_by(session_token: params[:session_token]).id
+    @transactions = Transaction.get_by_month(user_id, params[:month])
     render @transactions
   end
 
