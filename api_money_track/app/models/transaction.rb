d9c27714739af04c1c1fef04caa5cb773628a8f0
Mark LaRosa <mark.larosa2@gmail.com>
2016-09-21 21:20:34 -0400

    update transaction model

--- api_money_track/app/models/transaction.rb
+++ api_money_track/app/models/transaction.rb
@@ -5,4 +5,11 @@ class Transaction < ApplicationRecord
     foreign_key: :user_id,
     primary_key: :id
   )
+
+  def self.get_by_month(user_id, month)
+    transactions = Transaction.find_by(user_id: user_id)
+    transactions.select do |transaction|
+      transaction.committed.month == month
+    end
+  end
 end
