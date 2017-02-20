8a561908845108668b78cf77604868340d766794
Mark LaRosa <mark.larosa2@gmail.com>
2017-02-20 10:57:46 -0500

    add patch to cors

--- api_money_track/config/application.rb
+++ api_money_track/config/application.rb
@@ -14,7 +14,7 @@ module ApiMoneyTrack
     config.middleware.insert_before 0, Rack::Cors do
       allow do
         origins '*'
-        resource '*', :headers => :any, :methods => [:get, :post, :delete, :options]
+        resource '/api/*', :headers => :any, :methods => [:get, :post, :delete, :patch, :update, :options]
       end
     end
   end
