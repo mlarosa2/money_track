49ac32adbb781d7c224cf52dd6b08eef77bcfd7f
Mark LaRosa <mark.larosa2@gmail.com>
2017-02-20 00:14:02 -0500

    add update and delete methods

--- api_money_track/app/controllers/api/transactions_controller.rb
+++ api_money_track/app/controllers/api/transactions_controller.rb
@@ -3,6 +3,7 @@ class Api::TransactionsController < ApplicationController
     params.permit(:session_token, :month)
     user_id = User.find_by(session_token: params[:session_token]).id
     @transactions = Transaction.get_by_month(user_id, params[:month])
+    
     render :index
   end
 
@@ -16,6 +17,30 @@ class Api::TransactionsController < ApplicationController
     end
   end
 
+  def update
+    user_id = User.find_by(session_token: params[:transaction][:session_token]).id
+    @transaction = Transaction.find(params[:transaction][:id])
+
+    if @transaction.update({:name => params[:transaction][:name], :kind => params[:transaction][:kind], :amount => params[:transaction][:amount], :user_id => user_id, :month => params[:transaction][:month]})  
+      render @transaction
+    else
+      render json: @transaction.errors.full_messages, status: 412
+    end
+  end
+
+  def destroy 
+    @transaction = Transaction.find(params[:id])
+    user_id = User.find_by(session_token: params[:session_token]).id
+    transaction = @transaction
+
+    unless user_id.nil?
+      if @transaction.destroy
+        render json: transaction
+      else 
+        render json: @transaction.errors.full_messages, status: 412
+      end
+    end
+  end
 
   private
   def transaction_params
